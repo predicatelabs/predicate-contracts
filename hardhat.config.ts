2099b5b1407becd190b48bf4074f4574fbc122f4
Anmol Arora <a53arora@uwaterloo.ca>
2024-10-24 13:55:04 -0700

    added hardhat dep

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,34 @@
-import "@nomicfoundation/hardhat-foundry";
+import "hardhat-preprocessor";
+import fs from "fs";
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.12",
+  preprocess: {
+    eachLine: (hre: string) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
   paths: {
-    sources: "./src/",
+    sources: "./src/contracts",
     cache: "./cache_hardhat",
   }
 };
