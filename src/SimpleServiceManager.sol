32d16b289752951c6dfe51b7efd2ec1c17b63e5c
Anmol Arora <a53arora@uwaterloo.ca>
2025-07-15 12:27:45 -0700

    PRE-1996: Fixed event emission for policy syncing (#30)
    * Added policy
    
    * Test fixes
    
    * Update src/SimpleServiceManager.sol
    
    Co-authored-by: Jesse <31524583+Jesse-Sawa@users.noreply.github.com>
    
    * forge fmt
    
    ---------
    
    Co-authored-by: Jesse <31524583+Jesse-Sawa@users.noreply.github.com>

--- src/SimpleServiceManager.sol
+++ src/SimpleServiceManager.sol
@@ -165,7 +165,7 @@ contract SimpleServiceManager is ISimpleServiceManager, Initializable, Ownable2S
      * @param policyIDs Array of policy identifiers
      * @param thresholds Corresponding quorum thresholds for each policy
      */
-    function syncPolicies(string[] calldata policyIDs, uint32[] calldata thresholds) external onlyOwner {
+    function syncPolicyIDs(string[] calldata policyIDs, uint32[] calldata thresholds) external onlyOwner {
         require(
             policyIDs.length == thresholds.length, "Predicate.syncPolicies: policy IDs and thresholds length mismatch"
         );
@@ -179,9 +179,10 @@ contract SimpleServiceManager is ISimpleServiceManager, Initializable, Ownable2S
                 policyIDToThreshold[policyIDs[i]] = thresholds[i];
                 deployedPolicyIDs.push(policyIDs[i]);
                 emit PolicySynced(policyIDs[i]);
+            } else {
+                emit PolicySyncSkipped(policyIDs[i]);
             }
 
-            emit PolicySyncSkipped(policyIDs[i]);
             unchecked {
                 ++i;
             }
